f04a032622b70eb74b2bd5b526c89a214dd0b125
jikun.zhang <jikun.zhang@megatronix.co>
2022-01-21 11:55:43 +0800

    修复无法识别部分密码中带有特殊字符的错误
    Signed-off-by: jikun.zhang <jikun.zhang@megatronix.co>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"github.com/astaxie/beego"
@@ -51,7 +52,7 @@ func init() {
 }
 
 func main() {
-	version:="v1.0.0"
+	version := "v1.0.0"
 	logtype := beego.AppConfig.String("INIT::logtype")
 	if logtype == "console" {
 		logs.SetLogger(logtype)
@@ -69,9 +70,10 @@ func main() {
 	fsh := http.FileServer(http.Dir("proxystatic"))
 	http.Handle("/proxystatic/", http.StripPrefix("/proxystatic/", fsh))
 
-	logs.Info("[ "+time.Now().Format("2006/01/02 15:04:05")+" ] Start Proxy "+version+" On 0.0.0.0:"+beego.AppConfig.String("INIT::port"))
+	logs.Info("[ " + time.Now().Format("2006/01/02 15:04:05") + " ] Start Proxy " + version + " On 0.0.0.0:" + beego.AppConfig.String("INIT::port"))
 	http.ListenAndServe(":"+beego.AppConfig.String("INIT::port"), nil)
 }
+
 //登录
 func LdapLogin(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -79,29 +81,30 @@ func LdapLogin(w http.ResponseWriter, r *http.Request) {
 		t.Execute(w, nil)
 	} else {
 		//获取用户名和密码信息
-		username:=r.FormValue("username")
-		password:=r.FormValue("password")
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		password_base64 := base64.URLEncoding.EncodeToString([]byte(password))
 		//判断用户名密码是否正确
-		IsLogin,_:=LdapAuth(username,password)
-		if IsLogin  {
-			ck,_:=r.Cookie("olduri")
-			olduri:=ck.Value
-			maxage:= int(time.Hour * 24 / time.Second)
-			cookiename := &http.Cookie{Name: "username",Value: username, MaxAge:maxage}
-			cookiepass := &http.Cookie{Name: "password",Value: password, MaxAge:maxage}
-			cookieuri := &http.Cookie{Name: "olduri",Value: "", MaxAge:maxage}
+		IsLogin, _ := LdapAuth(username, password)
+		if IsLogin {
+			ck, _ := r.Cookie("olduri")
+			olduri := ck.Value
+			maxage := int(time.Hour * 24 / time.Second)
+			cookiename := &http.Cookie{Name: "username", Value: username, MaxAge: maxage}
+			cookiepass := &http.Cookie{Name: "password", Value: password_base64, MaxAge: maxage}
+			cookieuri := &http.Cookie{Name: "olduri", Value: "", MaxAge: maxage}
 			http.SetCookie(w, cookiename)
 			http.SetCookie(w, cookiepass)
 			http.SetCookie(w, cookieuri)
-			if olduri=="" {
-				http.Redirect(w,r,"/",302)
+			if olduri == "" {
+				http.Redirect(w, r, "/", 302)
 			} else {
-				http.Redirect(w,r,olduri,302)
+				http.Redirect(w, r, olduri, 302)
 			}
 			return
 		} else {
 			var errmsg interface{}
-			errmsg="用户名或密码错误，请重新输入！"
+			errmsg = "用户名或密码错误，请重新输入！"
 			t, _ := template.ParseFiles("views/login.html")
 			t.Execute(w, errmsg)
 			return
@@ -109,26 +112,27 @@ func LdapLogin(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+
 //管理员
 func LdapAdmin(w http.ResponseWriter, r *http.Request) {
-	IsLogin,_:=LdapAuthCheck(r)
+	IsLogin, _ := LdapAuthCheck(r)
 	if !IsLogin {
-		http.Redirect(w,r,"/ldaplogin",302)
+		http.Redirect(w, r, "/ldaplogin", 302)
 		return
 	}
 
 	//检查是否是admin用户
-	ck,err:=r.Cookie("username")
-	if err!=nil {
-		http.Redirect(w,r,"/ldaplogin",302)
+	ck, err := r.Cookie("username")
+	if err != nil {
+		http.Redirect(w, r, "/ldaplogin", 302)
 		return
 	}
-	username:=ck.Value
+	username := ck.Value
 	if IsNotAdmin(username) {
 		//w.Write([]byte("你不是管理员，禁止操作!"))
 		//return
 		var errmsg interface{}
-		errmsg="你不是管理员，禁止操作!"
+		errmsg = "你不是管理员，禁止操作!"
 		t, _ := template.ParseFiles("views/login.html")
 		t.Execute(w, errmsg)
 		return
@@ -140,39 +144,39 @@ func LdapAdmin(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			logs.Error(err)
 		}
-		t.Execute(w,  Template)
+		t.Execute(w, Template)
 
 	} else {
 		//获取表单信息
 		user := r.FormValue("user")
 		group := r.FormValue("group")
 		auth := r.FormValue("auth")
-		err:=AddUserAuth(user, group, auth)
-		if err!=nil {
-			logs.Error("添加用户权限失败：",err.Error())
+		err := AddUserAuth(user, group, auth)
+		if err != nil {
+			logs.Error("添加用户权限失败：", err.Error())
 		}
-		http.Redirect(w,r,"/ldapadmin",302)
+		http.Redirect(w, r, "/ldapadmin", 302)
 	}
 }
 
 func LdapAdminDel(w http.ResponseWriter, r *http.Request) {
-	IsLogin,_:=LdapAuthCheck(r)
+	IsLogin, _ := LdapAuthCheck(r)
 	if !IsLogin {
-		http.Redirect(w,r,"/ldaplogin",302)
+		http.Redirect(w, r, "/ldaplogin", 302)
 		return
 	}
 	//检查是否是admin用户
-	ck,err:=r.Cookie("username")
-	if err!=nil {
-		http.Redirect(w,r,"/ldaplogin",302)
+	ck, err := r.Cookie("username")
+	if err != nil {
+		http.Redirect(w, r, "/ldaplogin", 302)
 		return
 	}
-	username:=ck.Value
+	username := ck.Value
 	if IsNotAdmin(username) {
 		//w.Write([]byte("你不是管理员，禁止操作!"))
 		//return
 		var errmsg interface{}
-		errmsg="你不是管理员，禁止操作!"
+		errmsg = "你不是管理员，禁止操作!"
 		t, _ := template.ParseFiles("views/login.html")
 		t.Execute(w, errmsg)
 		return
@@ -180,50 +184,52 @@ func LdapAdminDel(w http.ResponseWriter, r *http.Request) {
 	Del_id, _ := strconv.Atoi(r.FormValue("id"))
 	err = DelUserAuth(Del_id)
 	if err != nil {
-		logs.Error("删除权限条目失败：",err)
+		logs.Error("删除权限条目失败：", err)
 	}
-	http.Redirect(w,r,"/ldapadmin",302)
+	http.Redirect(w, r, "/ldapadmin", 302)
 }
+
 //IsNotAdmin
 func IsNotAdmin(username string) bool {
-	AdminUsers:=strings.Split(beego.AppConfig.String("INIT::adminuser"), ",")
-	for _,Admin :=range AdminUsers {
-		if username==Admin {
+	AdminUsers := strings.Split(beego.AppConfig.String("INIT::adminuser"), ",")
+	for _, Admin := range AdminUsers {
+		if username == Admin {
 			return false
 		}
 	}
 	return true
 }
+
 //代理开始处理请求
 func ProxyToBackend(w http.ResponseWriter, r *http.Request) {
 	//判断用户是否登录过
-	IsLogin,Auths:=LdapAuthCheck(r)
-	if  !IsLogin {
-		maxage:= int(time.Hour * 24 / time.Second)
-		cookieuri := &http.Cookie{Name: "olduri",Value: r.RequestURI, MaxAge:maxage}
+	IsLogin, Auths := LdapAuthCheck(r)
+	if !IsLogin {
+		maxage := int(time.Hour * 24 / time.Second)
+		cookieuri := &http.Cookie{Name: "olduri", Value: r.RequestURI, MaxAge: maxage}
 		http.SetCookie(w, cookieuri)
-		http.Redirect(w,r,"/ldaplogin",302)
+		http.Redirect(w, r, "/ldaplogin", 302)
 		return
 	}
-	if Auths==nil {
+	if Auths == nil {
 		//w.Write([]byte("你没有权限访问，请联系管理员授权!"))
 		//return
 		var errmsg interface{}
-		errmsg="你没有权限访问，请联系管理员（"+beego.AppConfig.String("INIT::adminuser")+"）授权访问！"
+		errmsg = "你没有权限访问，请联系管理员（" + beego.AppConfig.String("INIT::adminuser") + "）授权访问！"
 		t, _ := template.ParseFiles("views/login.html")
 		t.Execute(w, errmsg)
 		return
 	}
 	//打印用户访问的页面
-	ck,_:=r.Cookie("username")
-	username:=ck.Value
-	logs.Info("[ "+time.Now().Format("2006/01/02 15:04:05")+" ] 用户：",username,"访问了URL：",r.RequestURI)
+	ck, _ := r.Cookie("username")
+	username := ck.Value
+	logs.Info("[ "+time.Now().Format("2006/01/02 15:04:05")+" ] 用户：", username, "访问了URL：", r.RequestURI)
 
 	client := &http.Client{}
-	var resp  = &http.Response{}
+	var resp = &http.Response{}
 
-	req, err := http.NewRequest(r.Method,beego.AppConfig.String("INIT::backend_url")+r.RequestURI,r.Body)
-	req.Header=r.Header
+	req, err := http.NewRequest(r.Method, beego.AppConfig.String("INIT::backend_url")+r.RequestURI, r.Body)
+	req.Header = r.Header
 	resp, err = client.Do(req)
 	if err != nil {
 		http.NotFound(w, r)
@@ -254,71 +260,73 @@ func ProxyToBackend(w http.ResponseWriter, r *http.Request) {
 
 //LDAP相关
 //检查cookie是否为登录状态
-func LdapAuthCheck(r *http.Request) (IsLogin bool,authorization *Authorization) {
-	ck,err:=r.Cookie("username")
-	if err!=nil {
-		return false,nil
+func LdapAuthCheck(r *http.Request) (IsLogin bool, authorization *Authorization) {
+	ck, err := r.Cookie("username")
+	if err != nil {
+		return false, nil
 	}
-	username:=ck.Value
-	ck,err=r.Cookie("password")
-	if err!=nil {
-		return false,nil
+	username := ck.Value
+	ck, err = r.Cookie("password")
+	if err != nil {
+		return false, nil
 	}
-	password:=ck.Value
-	return LdapAuth(username,password)
+	password_base64 := ck.Value
+	password_byte, _ := base64.URLEncoding.DecodeString(password_base64)
+	password := string(password_byte)
+	return LdapAuth(username, password)
 }
 
-func LdapAuth(user,passwd string) (IsLogin bool,authorization *Authorization)  {
+func LdapAuth(user, passwd string) (IsLogin bool, authorization *Authorization) {
 	//连接ldap
 	conn, err := ldap.Dial("tcp", beego.AppConfig.String("LDAP::ldap_url")+":"+beego.AppConfig.String("LDAP::ldap_port"))
 	if err != nil {
-		logs.Error("连接ldap:",err)
-		return false,nil
+		logs.Error("连接ldap:", err)
+		return false, nil
 	}
 	defer conn.Close()
 	//登录ldap
 	err = conn.Bind(beego.AppConfig.String("LDAP::ldap_login"), beego.AppConfig.String("LDAP::ldap_password"))
 	if err != nil {
-		logs.Error("登录ldap:",err)
-		return false,nil
+		logs.Error("登录ldap:", err)
+		return false, nil
 	}
 	//查询
-	filter:="(&(objectClass=person)(uid="+user+"))"
+	filter := "(&(objectClass=person)(uid=" + user + "))"
 	searchRequest := ldap.NewSearchRequest(
 		beego.AppConfig.String("LDAP::ldap_basedn"),
 		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
 		filter,
-		[]string{"dn","ou"},
+		[]string{"dn", "ou"},
 		nil,
 	)
 	sr, err := conn.Search(searchRequest)
 	if err != nil {
-		logs.Error("查询:",err)
-		return false,nil
+		logs.Error("查询:", err)
+		return false, nil
 	}
 
 	//查询结果
 	if len(sr.Entries) != 1 {
 		logs.Error("查询结果: search 0 entry")
-		return false,nil
+		return false, nil
 	}
 	//验证登录
 	userDN := sr.Entries[0].DN
 	err = conn.Bind(userDN, passwd)
 	if err != nil {
 		logs.Error("验证登录:bind password error: %v\n", err)
-		return false,nil
-	}else {
-		logs.Info("验证登录: 用户: ",user,"组: ",sr.Entries[0].GetAttributeValue("ou"),"恭喜登录成功")
-		return true,LdapAuthorization(user)
+		return false, nil
+	} else {
+		logs.Info("验证登录: 用户: ", user, "组: ", sr.Entries[0].GetAttributeValue("ou"), "恭喜登录成功")
+		return true, LdapAuthorization(user)
 	}
 }
 
 //授权,返回可以访问的Url
-func LdapAuthorization(user string) *Authorization  {
-	UserAuth,err:=GetUserAuth(user)
-	if err!=nil {
-		logs.Error("尝试查找用户权限信息失败：",err.Error())
+func LdapAuthorization(user string) *Authorization {
+	UserAuth, err := GetUserAuth(user)
+	if err != nil {
+		logs.Error("尝试查找用户权限信息失败：", err.Error())
 		return nil
 	}
 	return UserAuth
@@ -326,12 +334,12 @@ func LdapAuthorization(user string) *Authorization  {
 
 // 分类
 type Authorization struct {
-	Id      int		`orm:"pk;auto"`
-	User 	string 	`orm:"index"`
-	Group  	string
-	Auth 	string
-	Tpl     string 	`orm:"type(text)"`
-	Created time.Time	`orm:"auto_now"`
+	Id      int    `orm:"pk;auto"`
+	User    string `orm:"index"`
+	Group   string
+	Auth    string
+	Tpl     string    `orm:"type(text)"`
+	Created time.Time `orm:"auto_now"`
 }
 
 func GetAllUserAuths() ([]*Authorization, error) {
@@ -370,9 +378,9 @@ func AddUserAuth(user, group, auth string) error {
 		return err
 	}
 	Template_table := &Authorization{
-		User: 	user,
+		User:  user,
 		Group: group,
-		Auth:     auth,
+		Auth:  auth,
 	}
 	// 插入数据
 	_, err = o.Insert(Template_table)
